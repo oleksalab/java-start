6bf3571c01e0e678fca063244b0d3181bebfce00
oleksalab <41051979+oleksalab@users.noreply.github.com>
2018-07-18 11:16:42 +0300

    обновил описание задачи

--- src/Lesson04/Task04Ex04Level01.java
+++ src/Lesson04/Task04Ex04Level01.java
@@ -3,7 +3,7 @@ package Lesson04;
 import java.util.Scanner;
 
 /*
-наросовать фигуру в виде прямоугольника из `*`
+нарисовать фигуру в виде прямоугольника из `*`
  */
 public class Task04Ex04Level01
 {

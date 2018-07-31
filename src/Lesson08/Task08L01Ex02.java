c1b93ebc31e0bb8612edc6769203e39b382fb685
@oleksalab <alex.labunets@gmail.com>
2018-07-31 16:58:50 +0300

    добавил домашние задания урока 8

--- src/Lesson08/Task08L01Ex02.java
+++ src/Lesson08/Task08L01Ex02.java
@@ -1,5 +1,9 @@
 package Lesson08;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /*
 метод для сохранения в текстовый файл двухмерного массива целых чисел
  */
@@ -7,6 +11,58 @@ public class Task08L01Ex02 {
 
     public static void main(String[] args) {
 
+        String fileName = "out/L01Ex02-Array2xInt.txt";
 
+        int[][] matrix = {
+                {1, 2, 3, 4, 5},
+                {11, 22, 33, 44, 55},
+                {111, 222, 333, 444, 555}
+        };
+
+        // сохраняем массив в указанный файл
+        saveToFile(fileName, matrix);
+
+        System.out.println("Готово!");
+    }
+
+    // запись массива в файл
+    private static void saveToFile(String fileName, int[][] matrix) {
+
+        File file = new File(fileName);
+
+        try (PrintWriter writer = new PrintWriter(file)) {
+
+            for (int i = 0; i < matrix.length; i++) {
+
+                writer.append(ArraysToString(matrix[i])).append(System.lineSeparator());
+            }
+
+        } catch (FileNotFoundException fnfe) {
+
+            fnfe.printStackTrace();
+        }
+    }
+
+    // представление одномерного массива в виде строки
+    private static String ArraysToString(int[] list) {
+
+        String result;
+
+        if (list == null) result = "null";
+        else if (list.length == 0) result = "";
+        else {
+
+            StringBuilder line = new StringBuilder();
+
+            for (int item : list) {
+
+                line.append(item);
+                line.append(", ");
+            }
+
+            result = line.substring(0, line.lastIndexOf(","));
+        }
+
+        return result;
     }
 }
--- src/Lesson08/Task08L01Ex03.java
+++ src/Lesson08/Task08L01Ex03.java
@@ -1,12 +1,34 @@
 package Lesson08;
 
+import java.io.File;
+
 /*
-метод должен вывести на экран список всех каталогов внутри заданного параметром
+метод для вывода на экран списка всех каталогов внутри заданного параметром
  */
 public class Task08L01Ex03 {
 
     public static void main(String[] args) {
 
+        String folderName = "src";
 
+        // выводим каталоги из заданного
+        showFolders(folderName);
+
+    }
+
+    // вывод каталогов внутри заданного
+    private static void showFolders(String folderName) {
+
+        File folder = new File(folderName);
+        File[] items = folder.listFiles();
+
+        for (File item : items) {
+
+            // если текущий элемент каталог
+            if (item.isDirectory()) {
+                // выводим его на экран
+                System.out.println(item);
+            }
+        }
     }
 }
